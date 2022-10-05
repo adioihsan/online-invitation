3735f33f6f0732beb8501dbdf3b965a9e2351718
adioihsan <adioihsan@gmail.com>
2022-10-06 04:49:19 +0700

    initial project structure

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+function index(props) {
+    return (<>
+        <div>
+           <h1 className="text-fuchsia-600 font-semibold text-3xl">Hello Word</h1> 
+        </div>
+        </>
+    );
+}
+export default index;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.jsx",
+    "./layouts/**/.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
